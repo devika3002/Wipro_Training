8fdb895d05f81a621caa99dbee41120d1b158d43
Devika <devikaparimi3043@gmail.com>
2026-01-20 16:25:06 +0530

    RandomNumberGenerator

---
+++ Day10RandomNumberGen/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Security.Cryptography;
+class Program
+{
+    static void Main(string[] args)
+    {
+        //creating cryptographical secure random bytes
+        byte[] key = new byte[32];
+        using (RandomNumberGenerator rannumgen = RandomNumberGenerator.Create())
+        {
+            rannumgen.GetBytes(key);
+        }
+        //convert to base64 string
+        string secureKey = Convert.ToBase64String(key);
+        Console.WriteLine("Generated New Key: " +secureKey);
+    }
+}
