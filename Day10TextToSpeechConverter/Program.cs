6a5a694476286b9130ac4d838f3647bf4e54a2ea
Devika <devikaparimi3043@gmail.com>
2026-01-20 17:38:46 +0530

    Text to speech converter

---
+++ Day10TextToSpeechConverter/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Speech.Synthesis;
+
+namespace text_to_speech_convertor_app
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           SpeechSynthesizer synth = new SpeechSynthesizer();
+           synth.Speak("Hello, welcome to Text to Speech conversion app"); 
+        }
+    }
+}
